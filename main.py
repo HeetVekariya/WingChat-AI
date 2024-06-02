76424b26dd8f7020af860a0a7c3bc9a249bcc995
HEET VEKAIRYA <heetvekariya50@gmail.com>
2024-06-02 13:19:51 +0530

    chore: tools parameter passing

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from src.chatapp_crew.crew import HealthAdvisorCrew
 def run():
     inputs = {
         'data': 'src\chatapp_crew\data\\blood_report.pdf',
+        'search_tool': 'src\chatapp_crew\\tools\search_tool.py',
+        'pdf_tool': 'src\chatapp_crew\\tools\pdf_tool.py',
     }
     HealthAdvisorCrew().health_advisor_crew().kickoff(inputs)
 
